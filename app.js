46853a2508058621ce74b99e508f774bcdc2a996
Luke Igel <luke.igel@gmail.com>
2016-02-13 22:40:03 -0600

    integrates haversine algorithm sort of

--- app.js
+++ app.js
@@ -18,13 +18,29 @@ app.get('/messages/:id', function(req,res) {
 	res.json(message);
 });
 
+function getDistance(lat1, lat2, lon1, lon2){
+	var R = 6371000; //Earth's radius in metres
+	var lat1Rad = lat1.toRadians();
+	var lat2Rad = lat2.toRadians();
+	var deltaLat = (lat2-lat1).toRadians();
+	var deltaLon = (lon2-lon1).toRadians();
+
+	var a = Math.sin(deltaLat/2) * Math.sin(deltaLat/2) +
+	Math.cos(lat1Rad) * Math.cos(lat2Rad) *
+	Math.sin(deltaLon/2) * Math.sin(deltaLon/2);
+var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));  //angular distance in radians
+
+var d = R * c;	//distance between the two points (in meters?)
+}
+
 app.post('/messages', function(req,res){
 	var newMessage = {
 		message:req.body.message,
 		username:req.body.username,
 		timestamp: new Date(),
 		lat:req.body.lat,
-		lon:req.body.lon
+		lon:req.body.lon,
+		distance:req.body.;
 	};
 
 	messages.push(newMessage);
